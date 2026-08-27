842cdf27bbbb707918aa5e4e171ea052cae07ac5
Mohammad || m.devpath <m.devpath@outlook.com>
2026-08-27 07:27:53 +0300

    Connect MongoDB database to the system

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5500;
+
+await connectDB();
 
 app.use(express.json());
 
